d4c1584df4fb09840c2e07c365b2ff879c72216a
drkchiloll <samwomack_@me.com>
2016-05-25 13:51:33 -0500

    Inject HTTP_PROVIDER into the Project

--- app/main.ts
+++ app/main.ts
@@ -1,5 +1,6 @@
 import { bootstrap } from '@angular/platform-browser-dynamic';
-
+import { HTTP_PROVIDERS } from '@angular/http';
+import 'rxjs/Rx';
 import { AppComponent } from './components/app.component';
 
-bootstrap(AppComponent);
+bootstrap(AppComponent, [HTTP_PROVIDERS]);
